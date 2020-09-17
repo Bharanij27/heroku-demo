b04a6a92c83cc0e2bc89cf0847f757b64980148c
BHARATH KUMAR <bhavanisandhiya58@gmail.com>
2020-09-17 14:59:59 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const { response } = require("express");
+const fs = require('fs');
+
+app.use(bodyParser.json());
+
+app.get('/', function(req, res){
+    let current = new Date;
+    let fileName = ''+current.getDate()
+    fs.writeFile(fileName, current, function(err){
+        if(err) throw err;
+        console.log("Created and data added sucessfully");
+    })
+})
+
+app.listen(process.env.PORT || 3030, function () {
+  console.log("Server Started");
+});
